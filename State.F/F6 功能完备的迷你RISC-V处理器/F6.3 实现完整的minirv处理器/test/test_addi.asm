7ddb51d9315fdfab405f04ddfd13da385a06d7a6
qingzhixing <qzsqqmail@qq.com>
2026-03-23 15:15:12 +0800

    📦 NEW: 实现JA:R,正在添加测试用例

---
+++ State.F/F6 功能完备的迷你RISC-V处理器/F6.3 实现完整的minirv处理器/test/test_addi.asm
@@ -0,0 +1,7 @@
+00000000 <_start>:
+   0:	01400513          	addi	a0,zero,20
+   4:	ff400513          	addi	a0,zero,-12
+   8:	00a50593          	addi	a1,a0,10
+   c:	ff558593          	addi	a1,a1,-11
+  10:	7ff00613          	addi	a2,zero,2047
+  14:	00000067          	jalr	zero,0(zero)  # 自循环结束
