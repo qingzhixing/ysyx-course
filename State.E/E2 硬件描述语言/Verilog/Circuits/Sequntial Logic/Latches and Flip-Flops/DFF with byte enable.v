d17665e2d1510b3823baac501bf6b1078e0b6893
qingzhixing <qzsqqmail@qq.com>
2026-03-25 17:14:30 +0800

    📦 NEW: DFF with byte enable

---
+++ State.E/E2 硬件描述语言/Verilog/Circuits/Sequntial Logic/Latches and Flip-Flops/DFF with byte enable.v
@@ -0,0 +1,17 @@
+module top_module (
+    input clk,
+    input resetn,
+    input [1:0] byteena,
+    input [15:0] d,
+    output [15:0] q
+);
+
+	always @(posedge clk ) begin
+		if(resetn == 1) begin	
+			if (byteena[0]) q[7:0] = d[7:0];
+			if (byteena[1]) q[15:8] = d[15:8];
+		end
+		else q = 16'b0;
+	end
+
+endmodule
