125f3012c1fd6613f9cf1ee9c15d67793b8bf73b
qingzhixing <qzsqqmail@qq.com>
2026-03-26 17:05:15 +0800

    📦 NEW: Simple FSM 3 (synchronous reset)

---
+++ State.E/E2 硬件描述语言/Verilog/Circuits/Sequntial Logic/Finite State Machines/Simple FSM 3 (synchronous reset).sv
@@ -0,0 +1,30 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out); //
+
+	parameter A = 2'd0, B = 2'd1, C = 2'd2, D = 2'd3;
+	reg [1:0] current_state, next_state;
+
+    // State transition logic
+	always @(*) begin
+		case (current_state)
+			A: next_state = in? B:A;
+			B: next_state = in? B:C;
+			C: next_state = in? D:A;
+			D: next_state = in? B:C;
+			default: next_state = A;
+		endcase
+	end
+
+    // State flip-flops with asynchronous reset
+	always @(posedge clk) begin
+		if(reset) current_state <= A;
+		else current_state <= next_state;
+	end
+
+    // Output logic
+	assign out = (current_state == D);
+
+endmodule
