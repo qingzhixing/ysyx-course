0c11d87aba615a0fae88f08c44c5e7768ff34ada
qingzhixing <qzsqqmail@qq.com>
2026-03-27 15:44:13 +0800

    📦 NEW: Serial receiver with parity checking

--- State.E/E2 硬件描述语言/Verilog/Circuits/Sequntial Logic/Finite State Machines/Serial receiver with parity checking.sv
+++ State.E/E2 硬件描述语言/Verilog/Circuits/Sequntial Logic/Finite State Machines/Serial receiver with parity checking.sv
@@ -1,3 +1,6 @@
+
+// TODO: Wrong Answer!
+
 module top_module(
     input clk,
     input in,
@@ -8,19 +11,19 @@ module top_module(
 	parameter [3:0] IDLE_STATE  = 4'd0;
 	parameter [3:0] START_STATE = 4'd1;
 	parameter [3:0] DATA_STATE  = 4'd2;
-	parameter [3:0] END_STATE   = 4'd3;
-	parameter [3:0] ERROR_STATE = 4'd4;
+	parameter [3:0] ODD_STATE   = 4'd3;
+	parameter [3:0] END_STATE   = 4'd4;
+	parameter [3:0] ERROR_STATE = 4'd5;
 
 	reg [3:0] state, next_state;
+
 	reg [4:0] data_cnt;
 
-	wire odd;
+	reg odd;
 
-	// 例化parity模块
-    // 在IDLE/END/ERROR/START时持续reset，进入DATA后开始计数
     parity u_parity(
         .clk(clk),
-        .reset(reset || (next_state != DATA_STATE)),
+        .reset(reset || (state != DATA_STATE && state != ODD_STATE)),
         .in(in),
         .odd(odd)
     );
@@ -36,8 +39,11 @@ module top_module(
 				next_state = DATA_STATE;
 			end
 			DATA_STATE: begin
-				if(data_cnt < 9) next_state = DATA_STATE;
-				else if (in) next_state = END_STATE;
+				if(data_cnt < 8) next_state = DATA_STATE;
+				else next_state = ODD_STATE;
+			end
+			ODD_STATE: begin
+				if (in) next_state = END_STATE;
 				else next_state = ERROR_STATE;
 			end
 			END_STATE: begin
@@ -61,10 +67,8 @@ module top_module(
 			state <= next_state;
 			if(next_state == DATA_STATE) begin
 				data_cnt <= data_cnt + 5'b1;
-			end else data_cnt <= 0;
-
-			if(state == DATA_STATE && data_cnt < 5'd8)
 				out_byte[data_cnt - 1] <= in; 
+			end else data_cnt <= 0;
 		end
 	end	
 
