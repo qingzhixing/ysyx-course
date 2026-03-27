5fe180916f5318890894f5e36da7c6daf6482866
qingzhixing <qzsqqmail@qq.com>
2026-03-27 10:08:12 +0800

    📦 NEW: Lemmings 3

---
+++ State.E/E2 硬件描述语言/Verilog/Circuits/Sequntial Logic/Finite State Machines/Lemmings 3.sv
@@ -0,0 +1,70 @@
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    input ground,
+    input dig,
+    output walk_left,
+    output walk_right,
+    output aaah,
+    output digging ); 
+
+	// define states
+	parameter walking_left = 4'd0;
+	parameter walking_right = 4'd1;
+	parameter falling_left = 4'd2;
+	parameter falling_right = 4'd3;
+	parameter digging_left = 4'd4;
+	parameter digging_right = 4'd5;
+
+	// state regs
+	reg [3:0] state, next_state;
+
+	// State transition logic
+	always @(*) begin
+		case (state)
+			walking_left: begin
+				if(!ground) next_state = falling_left;
+				else if(dig) next_state = digging_left;
+				else if(bump_left) next_state = walking_right;
+				else next_state = walking_left;
+			end
+			walking_right: begin
+				if(!ground) next_state = falling_right;
+				else if(dig) next_state = digging_right;
+				else if(bump_right) next_state = walking_left;
+				else next_state = walking_right;
+			end
+			falling_left: begin
+				if(ground) next_state = walking_left;
+				else next_state = falling_left;
+			end
+			falling_right: begin
+				if(ground) next_state = walking_right;
+				else next_state = falling_right;
+			end
+			digging_left: begin
+				if(!ground) next_state = falling_left;
+				else next_state = digging_left;
+			end
+			digging_right: begin
+				if(!ground) next_state = falling_right;
+				else next_state = digging_right;
+			end
+		endcase
+	end
+
+	// State flip-flops with asynchronous reset
+	always @(posedge clk or posedge areset) begin
+		if(areset) state <= walking_left;
+		else state <= next_state;
+	end
+
+	// output logic
+	assign walk_left = (state == walking_left);
+	assign walk_right = (state == walking_right);
+	assign aaah = (state == falling_left) || (state == falling_right);
+	assign digging = (state == digging_left) || (state == digging_right);
+
+endmodule
