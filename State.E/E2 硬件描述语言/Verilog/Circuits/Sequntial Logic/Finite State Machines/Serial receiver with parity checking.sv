c5c4c84da4780f4b7826d6c4c7d50dc439b5fdd7
qingzhixing <qzsqqmail@qq.com>
2026-03-27 15:21:53 +0800

    📦 NEW: debugging

---
+++ State.E/E2 硬件描述语言/Verilog/Circuits/Sequntial Logic/Finite State Machines/Serial receiver with parity checking.sv
@@ -0,0 +1,73 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,    // Synchronous reset
+    output [7:0] out_byte,
+    output done
+);
+	parameter [3:0] IDLE_STATE  = 4'd0;
+	parameter [3:0] START_STATE = 4'd1;
+	parameter [3:0] DATA_STATE  = 4'd2;
+	parameter [3:0] END_STATE   = 4'd3;
+	parameter [3:0] ERROR_STATE = 4'd4;
+
+	reg [3:0] state, next_state;
+	reg [4:0] data_cnt;
+
+	wire odd;
+
+	// 例化parity模块
+    // 在IDLE/END/ERROR/START时持续reset，进入DATA后开始计数
+    parity u_parity(
+        .clk(clk),
+        .reset(reset || (next_state != DATA_STATE)),
+        .in(in),
+        .odd(odd)
+    );
+
+	// State Transition
+	always @(*) begin
+		case (state)
+			IDLE_STATE: begin
+				if(in) next_state = IDLE_STATE;
+				else next_state = START_STATE;
+			end
+			START_STATE: begin
+				next_state = DATA_STATE;
+			end
+			DATA_STATE: begin
+				if(data_cnt < 9) next_state = DATA_STATE;
+				else if (in) next_state = END_STATE;
+				else next_state = ERROR_STATE;
+			end
+			END_STATE: begin
+				if (in) next_state = IDLE_STATE;
+				else next_state = START_STATE;
+			end
+			ERROR_STATE: begin
+				if (in) next_state = IDLE_STATE;
+				else next_state = ERROR_STATE;
+			end
+			default: next_state = IDLE_STATE;
+		endcase
+	end
+
+	// State Flip-Flop
+	always @(posedge clk) begin
+		if(reset) begin
+			state <= IDLE_STATE;
+			data_cnt <= 0;
+		end else begin
+			state <= next_state;
+			if(next_state == DATA_STATE) begin
+				data_cnt <= data_cnt + 5'b1;
+			end else data_cnt <= 0;
+
+			if(state == DATA_STATE && data_cnt < 5'd8)
+				out_byte[data_cnt - 1] <= in; 
+		end
+	end	
+
+	// Output
+	assign done = (state == END_STATE) && odd;
+endmodule
